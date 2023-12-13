248ba5b67a9f75e44e6e0ba73a5fe16e5b64bb20
madkar85 <madeleine.karlsson@outlook.com>
2023-12-13 15:53:52 +0100

    refactoring and started to check if bomb and to calculate adjecent bombs. not done yet

--- project_skeleton/cell.py
+++ project_skeleton/cell.py
@@ -1,19 +1,18 @@
 import pygame
 import random
 
-
 class Cell:
     """This file contains the cell class representing each square in the game"""
 
-    def __init__(self, x, y, width, height, bomb_chance):
-        self.x = x
-        self.y = y
+    def __init__(self, cell_pos_x, cell_pos_y, width, height, bomb_chance, font):
+        self.x = cell_pos_x * width
+        self.y = cell_pos_y * height
         self.width = width
         self.height = height
-        self.color = (0, 64, 0)  # RGB color
         self.cell_thickness = 2
-        self.neighbouring_bombs = 0
+        self.neighbouring_bombs = 7
         self.selected = False
+        self.font = font
 
         self.cell_center = (
             self.x + self.width // 2,
@@ -23,22 +22,37 @@ class Cell:
             random.random() < bomb_chance
         )  # each cell has a chance of being a bomb
 
+        # for debugging, draw gray outline for bomb cells
+        if self.bomb:
+            self.color = (128, 128, 128)  # RGB color
+        else:
+            self.color = (0, 64, 0)
+
     def draw(self, screen):
         """This method is called in the main.py files draw_cells fkn"""
         """Ritar ut enskilda cellerna och senare bomberna och siffrorna"""
         # Hint: Should draw each cell, i.e something to do with pygame.draw.rect
-        pygame.draw.rect( screen, self.color, (
-            self.x, 
-            self.y, 
-            self.width, 
-            self.height), 
-            self.cell_thickness
-            )
+        rect = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(screen, self.color, rect, self.cell_thickness)
 
+        if self.selected:
+            # draw number in font
+            text = self.font.render(str(self.neighbouring_bombs), True, (255, 255, 255))
+            text_rect = text.get_rect()
+            text_rect.center = self.cell_center
+            screen.blit(text, text_rect)
         # Later on in the assignment it will do more as well such as drawing X for bombs or writing digits
         # Important: Remember that pygame starts with (0,0) coordinate in upper left corner!
-        #screen.fill("black")
-        pass
+
+    def on_click(self):
+        self.selected = True
+
 
     def update(self):
         pass
+
+    def get_cell_center(self):
+        """This method is called in the main.py files calc_cell_pos fkn"""
+        """Returnerar cellens center"""
+        return self.cell_center
+    
--- project_skeleton/main.py
+++ project_skeleton/main.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 from cell import Cell
-from calcs import measure_distance
 
 """ This is the main file you work on for the project"""
 
@@ -17,32 +16,26 @@ CELL_WIDTH = CELL_HEIGHT = CELL_SIZE  # Probably not needed, just use cell_size
 
 SCREEN_WIDTH, SCREEN_HEIGHT = READJUSTED_SIZE, READJUSTED_SIZE
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+font = pygame.font.Font(None, CELL_SIZE)
 
 pygame.display.set_caption("MineSweeper")
 
-font = pygame.font.Font(None, CELL_SIZE)
-text_surface = font.render('8', True, (255, 255, 255))
-text_rect = text_surface.get_rect()
 
-rendered_chars = []
 cells = []
-
+#board = []
 
 def create_cells():
     """This function is meant to initially generate all the cells and create the boundaries"""
-    x = 0
-    y = 0
     width = CELL_SIZE
     height = CELL_SIZE
     
-    for _column in range(amount_of_cells):
-        for _row in range(amount_of_cells):
-            cell = Cell(x,y,width,height,bomb_chance)
-            cells.append(cell)
-            x += width
+    for cell_y in range(amount_of_cells):
+        cells.append([])
 
-        y += height
-        x = 0
+        for cell_x in range(amount_of_cells):
+            cell = Cell(cell_x, cell_y, width, height, bomb_chance, font)
+
+            cells[cell_y].append(cell)
    
     # This is a good base to go from (think about it thoroughly before you code!! We want to create 16x16 list with each object being a cell):
     #for a_row in range(amount_of_cells):
@@ -56,44 +49,24 @@ def create_cells():
 def draw_cells():
     """In this function we want to draw each cell, i.e call upon each cells .draw() method!"""
     # Hint: take inspiration from the forloop in create_cells to loop over all the cells
-    for cell in cells:
-         cell.draw(screen)
-
-
-def draw_bombs():
-    pass
-
-
-def draw_character_in_cell():
-    """Draws a number in a cell"""
-    # få mitten av cellen : 
-    
-    for text_surface, text_rect in rendered_chars:
-        #cell_center_x = cells[0] + CELL_SIZE // 2
-        #cell_center_y = cells[1] + CELL_SIZE // 2
-        #cell_center = cell_center_x + cell_center_y
-        screen.blit(text_surface, text_rect)
-
+    for col in cells:
+        for cell in col:
+            cell.draw(screen)
 
 
 def draw():
     """This function handles all the drawings to the screen, such as drawing rectangles, objects etc"""
     screen.fill((0, 0, 0))
     draw_cells()
-    draw_character_in_cell()
-    
-   
 
 
-def cacl_cell_pos(mouse_pos):
+def calc_cell_pos(position):
     """This function calculates the cell position of the mouse"""
-    x = mouse_pos[0] // CELL_SIZE
-    y = mouse_pos[1] // CELL_SIZE 
-    draw_character_in_cell(cells[x,y])
-    
-    #return cells[x,y]
+    # Hint: take inspiration from the forloop in create_cells to loop over all the cells
+    center_x = position[0] // CELL_SIZE
+    center_y = position[1] // CELL_SIZE
 
-    pass
+    return (center_x, center_y)
 
 
 def event_handler(event):
@@ -102,15 +75,14 @@ def event_handler(event):
         terminate_program()
     
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        clicked_x, clicked_y = pygame.mouse.get_pos()
-        text_rect.centerx = clicked_x
-        text_rect.centery = clicked_y
+        cell_grid_pos_x, cell_grid_pos_y = calc_cell_pos(pygame.mouse.get_pos())
+        cell = cells[cell_grid_pos_y][cell_grid_pos_x]
+        if cell.bomb:
+            terminate_program()
+            
+        cell.on_click()
 
-        rendered_chars.append((text_surface, text_rect.copy()))
 
-        print(clicked_x, clicked_y)
-        #cacl_cell_pos(mouse_pos)
-        #draw_cells(mouse_pos)
 
 
 def run_setup():
@@ -128,15 +100,11 @@ def main():
     run_setup()
 
     while True:
-
-
         for event in pygame.event.get():
             event_handler(event)
 
         draw()
         pygame.display.flip()
-
-    terminate_program()
 
 
 if __name__ == "__main__":
